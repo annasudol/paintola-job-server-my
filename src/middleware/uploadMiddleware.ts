6007f715b6f55df067cdb0f5198368232e20928d
Anna <32278291+annasudol@users.noreply.github.com>
2025-05-13 10:13:17 +0200

    middleware

--- src/middleware/uploadMiddleware.ts
+++ src/middleware/uploadMiddleware.ts
@@ -2,10 +2,10 @@ import multer from "multer"
 
 /**
  * Multer configuration for uploading remix images.
- * Uses in-memory storage and limits file size to 5MB.
+ * Uses in-memory storage and limits file size to 20MB.
  */
 
 export const remixUpload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 10 * 1024 * 1024 }, // 20MB limit
 }).single("image_file")
